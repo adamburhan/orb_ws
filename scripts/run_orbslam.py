408bd82fd35e4fadd8699ef02441e1d1af263070
adamburhan <adam14011999@gmail.com>
2025-06-24 18:51:32 -0400

    update dockerfile

--- scripts/run_orbslam.py
+++ scripts/run_orbslam.py
@@ -2,34 +2,46 @@ import os
 import subprocess
 
 DATASETS = {
-    #"euroc": ["MH_01_easy", "MH_04_difficult"],
-    "tum_vi": [
-        # "dataset-magistrale2_512_16",
-        # "dataset-magistrale3_512_16",
-        # "dataset-magistrale4_512_16",
-        # "dataset-magistrale5_512_16",
-        # "dataset-magistrale6_512_16",
-        "dataset-outdoors1_512_16",
-        "dataset-outdoors2_512_16",
-        "dataset-outdoors3_512_16",
-        "dataset-outdoors4_512_16",
-        "dataset-outdoors5_512_16",
-        "dataset-outdoors6_512_16",
-        "dataset-outdoors7_512_16",
-        "dataset-outdoors8_512_16",
-        # "dataset-room1_512_16",
-        # "dataset-room2_512_16",
-        # "dataset-room3_512_16",
-        # "dataset-room4_512_16",
-        # "dataset-room5_512_16",
-        # "dataset-room6_512_16",
-        # "dataset-slides1_512_16",
-        # "dataset-slides2_512_16",
-        "dataset-slides3_512_16"
-        ]
+    "euroc": [
+        "MH_01_easy", 
+        "MH_03_medium",
+        "MH_04_difficult",
+        "MH_05_difficult",
+        "V1_01_easy",
+        "V1_03_difficult",
+        "V2_01_easy",
+        "V2_02_medium",
+        "V2_03_difficult",
+        "MH_02_easy",
+        "V1_02_medium",
+        ],
+    # "tum_vi": [
+    #     # "dataset-magistrale2_512_16",
+    #     # "dataset-magistrale3_512_16",
+    #     # "dataset-magistrale4_512_16",
+    #     # "dataset-magistrale5_512_16",
+    #     # "dataset-magistrale6_512_16",
+    #     "dataset-outdoors1_512_16",
+    #     "dataset-outdoors2_512_16",
+    #     "dataset-outdoors3_512_16",
+    #     "dataset-outdoors4_512_16",
+    #     "dataset-outdoors5_512_16",
+    #     "dataset-outdoors6_512_16",
+    #     "dataset-outdoors7_512_16",
+    #     "dataset-outdoors8_512_16",
+    #     # "dataset-room1_512_16",
+    #     # "dataset-room2_512_16",
+    #     # "dataset-room3_512_16",
+    #     # "dataset-room4_512_16",
+    #     # "dataset-room5_512_16",
+    #     # "dataset-room6_512_16",
+    #     # "dataset-slides1_512_16",
+    #     # "dataset-slides2_512_16",
+    #     "dataset-slides3_512_16"
+    #     ]
 }
 
-MODES = ["mono_inertial"]
+MODES = ["mono"]
 
 docker_image = "orb_slam3_ros"
 data_path = os.path.abspath("data")
